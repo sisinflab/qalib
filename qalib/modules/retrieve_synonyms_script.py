314d1d268dda0d48bca6d4794819a404720d1bfc
Angelo Schiavone <schiavone.angelo27@gmail.com>
2019-04-01 17:24:52 +0200

    Minor bugs fixed

--- qalib/modules/retrieve_synonyms_script.py
+++ qalib/modules/retrieve_synonyms_script.py
@@ -2,21 +2,6 @@ import urllib.request
 from urllib.parse import quote
 from bs4 import BeautifulSoup
 
-
-def retrieve_type(word, html_page, first_syn):
-    localSoup = BeautifulSoup(html_page, "html5lib")
-
-    if localSoup.find('span', attrs={'class': 'word-type'}):
-        type = localSoup.find('span', attrs={'class': 'word-type'}).contents[0]
-
-    else:
-        url = "http://synonyms.woxikon.com/it/" + first_syn
-        data = urllib.request.urlopen(url).read()
-        type = retrieve_type(word, data, first_syn)
-
-    return type
-
-
 def retrieve_syn_list(word, language):
 
     url = "http://synonyms.woxikon.com/" + quote(language) + "/" + quote(word)
@@ -31,20 +16,10 @@ def retrieve_syn_list(word, language):
 
     if 'Your search did not match any results' not in soup_text:
 
-        for div in soup.findAll('span', attrs={'class': 'synonyms-list-first-content'}):
+        for div in soup.findAll('div', attrs={'class': 'synonyms-list-content'}):
 
-            for a in div.find_all('a'):
-                if not a.contents[0] in syn_vect:
-                    syn_vect.append(a.contents[0])
-
-        div = soup.find('ol', attrs={'class': 'synonyms-list'})
-        if div:
-            for li in div.find_all('li', attrs={'class': 'synonyms-list-item clear-content'}):
-
-                div_list = li.find('div', attrs={'class': 'synonyms-list-content'})
-
-                for a in div_list.find_all('a'):
-                    if not a.contents[0] in syn_vect:
-                        syn_vect.append(a.contents[0])
+            for span in div.find_all('span', attrs={'class': 'text-black'}):
+                if not span.text.strip() in syn_vect:
+                    syn_vect.append(span.text.strip())
 
     return syn_vect
--- qalib/setup.sh
+++ qalib/setup.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/bash
+#!/bin/bash
 
 pip3 install bs4
 pip3 install html5
